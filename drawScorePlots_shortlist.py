767a72a6c763f033f006113312cecc49cc03debc
Elliott Kauffman <elmaka8700@gmail.com>
2024-02-28 09:26:55 -0600

    version with all from shortlist on same plot

--- drawScorePlots_shortlist.py
+++ drawScorePlots_shortlist.py
@@ -1,7 +1,18 @@
 import ROOT
 import argparse
 import numpy as np
-from paperSampleBuilder import samples
+
+sample_names = ["GluGluHToBB_M-125_TuneCP5_13p6TeV_powheg-pythia8",
+                "GluGluHToGG_M-125_TuneCP5_13p6TeV_powheg-pythia8",
+                "HHHTo6B_c3_0_d4_0_TuneCP5_13p6TeV_amcatnlo-pythia8",
+                "HTo2LongLivedTo4b_MH-350_MFF-80_CTau-1000mm_TuneCP5_13p6TeV-pythia8",
+                "SUEP",
+                "SUSYGluGluToBBHToBB_NarrowWidth_M-350_TuneCP5_13p6TeV-pythia8",
+                "ttHto2C_M-125_TuneCP5_13p6TeV_powheg-pythia8",
+                "VBFHToInvisible_M-125_TuneCP5_13p6TeV_powheg-pythia8",
+                "TT_TuneCP5_13p6TeV_powheg-pythia8"]
+
+sample_colors = [46, 38, 30, 28, 42, 49, 39, 32, 41]
 
 def createCMSLabel():
 	cmsLatex = ROOT.TLatex()
@@ -16,51 +27,47 @@ def main(input_file, output_dir, cicada_name):
 
 	f = ROOT.TFile(input_file)
 
-	sample_names = list(samples.keys())
-	if 'ZeroBias' in sample_names:
-		sample_names.remove('ZeroBias')
-	
+	c1 = ROOT.TCanvas("c1", "Anomaly Score", 1000,600)
+	hist_zerobias = f.Get(f"anomalyScore_ZeroBias_{cicada_name}")
+
+	hist_zerobias.GetYaxis().SetRangeUser(1,1e9)
+	hist_zerobias.GetXaxis().SetRangeUser(0,256)
+	hist_zerobias.SetMarkerColor(1)
+	hist_zerobias.SetMarkerStyle(20)
+	hist_zerobias.SetStats(0)
+	hist_zerobias.SetTitle("")
+	hist_zerobias.Draw("e")
+
+	legend = ROOT.TLegend(0.1,0.7,0.9,0.9)
 
 	for i in range(len(sample_names)):
 		hist_sample = f.Get(f"anomalyScore_{sample_names[i]}_{cicada_name}")
-		hist_zerobias = f.Get(f"anomalyScore_ZeroBias_{cicada_name}")
 
-		c1 = ROOT.TCanvas("c1", "Anomaly Score", 1000,600)
-		hist_sample.SetMarkerColor(4)
+	for i in range(len(sample_names)):
+		hist_sample = f.Get(f"anomalyScore_{sample_names[i]}_{cicada_name}")
+
+		hist_sample.SetMarkerColor(sample_colors[i])
 		hist_sample.SetMarkerStyle(20)
-		hist_zerobias.GetXaxis().SetTitle("CICADA Score Threshold");
-		hist_zerobias.GetYaxis().SetTitle("Counts");
 		hist_sample.SetStats(0)
 		hist_sample.SetTitle("")
-	
-		hist_zerobias.GetYaxis().SetRangeUser(1,1e9)
-		hist_zerobias.GetXaxis().SetRangeUser(0,256)	
-		hist_zerobias.SetMarkerColor(2)
-		hist_zerobias.SetMarkerStyle(20)
-		hist_zerobias.SetStats(0)
-		#hist_zerobias.Draw("b same")
-		hist_zerobias.SetTitle("")
-		hist_zerobias.Draw("e")
-
 		hist_sample.Draw("e same")
 
-		cmsLatex = createCMSLabel()
-		cmsLatex.DrawLatex(0.1,0.92, "#font[61]{CMS} #font[52]{Preliminary}")
+	cmsLatex = createCMSLabel()
+	cmsLatex.DrawLatex(0.1,0.92, "#font[61]{CMS} #font[52]{Preliminary}")
 
-		legend = ROOT.TLegend(0.1,0.7,0.9,0.9)
-		legend.AddEntry(hist_sample, f"{sample_names[i]}", "PE")
-		legend.AddEntry(hist_zerobias, "ZeroBias", "PE")
-		legend.SetTextSize(0.025)
-		legend.SetBorderSize(0)
-		legend.SetFillStyle(0)
-		legend.Draw()
+	legend.AddEntry(hist_sample, f"{sample_names[i]}", "PE")
+	legend.AddEntry(hist_zerobias, "ZeroBias", "PE")
+	legend.SetTextSize(0.025)
+	legend.SetBorderSize(0)
+	legend.SetFillStyle(0)
+	legend.Draw()
 
-		c1.SetLogy()
-		c1.Draw()
-		c1.SaveAs(f"{output_dir}/scorehist_{sample_names[i]}_{cicada_name}.png")
-		c1.Close()
+	c1.SetLogy()
+	c1.Draw()
+	c1.SaveAs(f"{output_dir}/scorehist_{sample_names[i]}_{cicada_name}.png")
+	c1.Close()
 		
-		hist_sample = f.Get(f"rate_{sample_names[i]}_{cicada_name}")
+	'''	hist_sample = f.Get(f"rate_{sample_names[i]}_{cicada_name}")
 		hist_zerobias = f.Get(f"rate_ZeroBias_{cicada_name}")
 		
 		c3= ROOT.TCanvas("c2","Rate",1000,600)
@@ -95,7 +102,7 @@ def main(input_file, output_dir, cicada_name):
 		c3.SetLogy()
 		c3.Draw()
 		c3.SaveAs(f"{output_dir}/rate_{sample_names[i]}_{cicada_name}.png")
-		c3.Close()
+		c3.Close() '''
 
 if __name__ == "__main__":
 
