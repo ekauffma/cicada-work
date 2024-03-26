8315b0947123a41469fcf06d25e88d68b8a448ad
Elliott Kauffman <elmaka8700@gmail.com>
2024-03-26 12:04:03 -0500

    cleaned up and commented createScorePlots_compare

--- createScorePlots_compare.py
+++ createScorePlots_compare.py
@@ -1,3 +1,10 @@
+/*--------------------------------------------------------------------*/
+/* createScorePlots_compare.py                                        */
+/* Author: Elliott Kauffman                                           */
+/* Creates histograms of the CICADA scores for noise-suppressed and   */
+/* non-noise suppressed versions                                      */
+/*--------------------------------------------------------------------*/
+
 import ROOT
 import argparse
 import numpy as np
@@ -5,81 +12,66 @@ from paperSampleBuilder import samples
 
 def main(nBins):
 
+    # get list of sample names and remove ZeroBias
 	sample_names = list(samples.keys())
 	if 'ZeroBias' in sample_names:
 		sample_names.remove('ZeroBias')
 
+    # filter training events out of ZeroBias
 	zero_bias = samples['ZeroBias'].getNewDataframe()
 	zero_bias = zero_bias.Filter('lumi % 2 == 1')
 
-	cicada_names = ["CICADA_v2p2p0"]#,"CICADA_v1p2p0N","CICADA_v2p2p1N"]
+    # names of cicada versions
+	cicada_names = ["CICADA_v1p2p0",
+                    "CICADA_v2p2p0",
+                    "CICADA_v1p2p0N",
+                    "CICADA_v2p2p0N",
+                    "CICADA_v1p2p1",
+                    "CICADA_v2p2p1",
+                    "CICADA_v1p2p1N",
+                    "CICADA_v2p2p1N"]
 
+    # min and max scores for histograms
 	min_score = 0.0
 	max_score = 1024.0
 
+    # create one output file for each cicada version
 	for i in range(len(cicada_names)):
 
-		output_file = ROOT.TFile(f'hists_compare_240312_{cicada_names[i]}.root', 'RECREATE')
+        # create output ROOT file
+		output_file = ROOT.TFile(
+            f'hists_compare_240312_{cicada_names[i]}.root',
+            'RECREATE'
+        )
 
 		# make hists for zerobias
-		histModel = ROOT.RDF.TH1DModel(f"anomalyScore_ZeroBias_{cicada_names[i]}", f"anomalyScore_ZeroBias_{cicada_names[i]}", nBins, min_score, max_score)
+		histModel = ROOT.RDF.TH1DModel(
+            f"anomalyScore_ZeroBias_{cicada_names[i]}",
+            f"anomalyScore_ZeroBias_{cicada_names[i]}",
+            nBins,
+            min_score,
+            max_score
+        )
 		hist = zero_bias.Histo1D(histModel, f"{cicada_names[i]}_score")
 		hist.Write()
-		
-		'''
-		eff = ROOT.TH1D(f"efficiency_ZeroBias_{cicada_names[i]}", f"efficiency_ZeroBias_{cicada_names[i]}", hist.GetNbinsX(), hist.GetXaxis().GetXmin(), hist.GetXaxis().GetXmax())
-		scoreplot_int = float(hist.Integral())
 
-		for j in range(1, hist.GetNbinsX()+1):
-			scoreplot_int_current = float(hist.Integral(j, hist.GetNbinsX()))
-			uncertainty = np.sqrt(scoreplot_int_current)
-			scoreplot_int_current = scoreplot_int_current/scoreplot_int
-			uncertainty = uncertainty/scoreplot_int
-			eff.SetBinContent(j,scoreplot_int_current)
-			eff.SetBinError(j, uncertainty)
-
-		rate = eff.Clone(f"rate_ZeroBias_{cicada_names[i]}")
-		rate.Scale(2544.0 * 11245e-3) # convert efficiency to rate
-		rate.Write()
-		'''
-
-		for k in range(len(sample_names)):				
+        # iterate through each sample
+		for k in range(len(sample_names)):
 			print(sample_names[k])
+
 			# make score plot
-			histModel = ROOT.RDF.TH1DModel(f"anomalyScore_{sample_names[k]}_{cicada_names[i]}", f"anomalyScore_{sample_names[k]}_{cicada_names[i]}", 
-							nBins, min_score, max_score)
-			rdf = samples[sample_names[k]].getNewDataframe()			
+			histModel = ROOT.RDF.TH1DModel(
+                f"anomalyScore_{sample_names[k]}_{cicada_names[i]}",
+                f"anomalyScore_{sample_names[k]}_{cicada_names[i]}",
+                nBins,
+                min_score,
+                max_score
+            )
+			rdf = samples[sample_names[k]].getNewDataframe()
 			hist = rdf.Histo1D(histModel, f"{cicada_names[i]}_score")
 			hist.Write()
-			'''		
-			# make efficiency plot
-			eff = ROOT.TH1D(f"efficiency_{sample_names[k]}_{cicada_names[i]}",
-					f"efficiency_{sample_names[k]}_{cicada_names[i]}",
-					hist.GetNbinsX(), 
-					hist.GetXaxis().GetXmin(),
-					hist.GetXaxis().GetXmax())
 
-			# total integral
-			scoreplot_int = float(hist.Integral())
-
-			# compute value for each threshold
-			for j in range(1, hist.GetNbinsX()+1):
-				scoreplot_int_current = float(hist.Integral(j, hist.GetNbinsX()))
-				uncertainty = np.sqrt(scoreplot_int_current)
-				scoreplot_int_current = scoreplot_int_current/scoreplot_int
-				uncertainty = uncertainty/scoreplot_int
-
-				eff.SetBinContent(j,scoreplot_int_current)
-				eff.SetBinError(j, uncertainty)
-		
-			eff.Write()
-
-			# make rate plot
-			rate = eff.Clone(f"rate_{sample_names[k]}_{cicada_names[i]}")
-			rate.Scale(2544.0 * 11245e-3) # convert efficiency to rate
-			rate.Write()
-			'''
-		
+        # write and close file
 		output_file.Write()
 		output_file.Close()
 
