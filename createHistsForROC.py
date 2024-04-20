9297d17bad505b2d6fa6cfffe9204324d604916f
Elliott Kauffman <elmaka8700@gmail.com>
2024-04-20 14:40:26 -0500

    finished updating loop for samples

--- createHistsForROC.py
+++ createHistsForROC.py
@@ -12,24 +12,24 @@ from paperSampleBuilder import samples
 def main(nBins):
 
     # get list of sample names and remove ZeroBias
-	sample_names = list(samples.keys())
-	if 'ZeroBias' in sample_names:
-		sample_names.remove('ZeroBias')
+    sample_names = list(samples.keys())
+    if 'ZeroBias' in sample_names:
+        sample_names.remove('ZeroBias')
 
     # get zerobias and filter out training events
-	zero_bias = samples['ZeroBias'].getNewDataframe()
-	zero_bias_test = zero_bias.Filter('lumi % 2 == 1')
+    zero_bias = samples['ZeroBias'].getNewDataframe()
+    zero_bias_test = zero_bias.Filter('lumi % 2 == 1')
     zero_bias_train = zero_bias.Filter('lumi % 2 == 0')
 
     # names of CICADA models
-	cicada_names = ["CICADA_v1p2p2",
+    cicada_names = ["CICADA_v1p2p2",
                     "CICADA_v2p2p2",
                     "CICADA_v1p2p2N",
                     "CICADA_v2p2p2N"]
 
     # minimum and maximum scores for histogram
-	min_score_cicada = 0.0
-	max_score_cicada = 256.0
+    min_score_cicada = 0.0
+    max_score_cicada = 1024.0
     min_score_ht = 0.0
     max_score_ht = 1500.0
 
@@ -96,17 +96,47 @@ def main(nBins):
     # create and write hists for each sample
     for k in range(len(sample_names)):
         print(f"Sample: {sample_names[k]}")
-        output_file = ROOT.TFile(f"hists_240420_{sample_names[k]}.root". "RECREATE")
+        output_file = ROOT.TFile(f"hists_240420_{sample_names[k]}.root", "RECREATE")
 
+        rdf = samples[sample_names[k]].getNewDataframe()
 
+        # create and fill CICADA score histograms
+        for i in range(len(cicada_names)):
+            print("    CICADA VERSION: ", cicada_names[i])
 
+            histModel = ROOT.RDF.TH1DModel(
+                f"anomalyScore_{sample_names[k]}_{cicada_names[i]}",
+                f"anomalyScore_{sample_names[k]}_test_{cicada_names[i]}",
+                nBins,
+                min_score_cicada,
+                max_score_cicada
+            )
+            hist = rdf.Histo1D(histModel, f"{cicada_names[i]}_score")
+            hist.Write()
 
+        # create and fill HT histogram
+        print("   HT")
+        rdf = rdf.Define("sum_mask", "(sumBx==0) && (sumType==1)")
+        rdf = rdf.Define("HT", "sumEt[sum_mask]")
+
+        histModel = ROOT.RDF.TH1DModel(
+            f"HT_{sample_names[k]}_{cicada_names[i]}",
+            f"HT_{sample_names[k]}_{cicada_names[i]}",
+            nBins,
+            min_score_ht,
+            max_score_ht
+        )
+        hist = rdf.Histo1D(histModel, "HT")
+        hist.Write()
+
+        output_file.Write()
+        output_file.Close()
 
 
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser(
-        description="This program creates CICADA score plots"
+        description="This program creates CICADA score and HT histograms"
     )
 	parser.add_argument(
         "-n",
--- paperSampleBuilder.py
+++ paperSampleBuilder.py
@@ -76,25 +76,29 @@ for date in sampleDates:
         )
 
 # Add the SUEPS by hand
-samplePaths["SUEP"] = "/hdfs/store/user/aloeliger/SUEP_Paper_Analysis/08Mar2024/"
+samplePaths["SUEP"] = "/hdfs/store/user/aloeliger/SUEP_Paper_Analysis/05Feb2024/"
 
 # the ROOT trees we care about
 treeNames = [
     'l1EventTree/L1EventTree',
-    #'l1CaloTowerEmuTree/L1CaloTowerTree',
+    'l1CaloTowerEmuTree/L1CaloTowerTree',
     'l1UpgradeTree/L1UpgradeTree',
-    'CICADA_v1p2p0_Ntuplizer/CICADA_v1p2p0',
-    'CICADA_v2p2p0_Ntuplizer/CICADA_v2p2p0',
-    'CICADA_v1p2p0N_Ntuplizer/CICADA_v1p2p0N',
-    'CICADA_v2p2p0N_Ntuplizer/CICADA_v2p2p0N',
-    'CICADA_v1p2p1_Ntuplizer/CICADA_v1p2p1',
-    'CICADA_v2p2p1_Ntuplizer/CICADA_v2p2p1',
-    'CICADA_v1p2p1N_Ntuplizer/CICADA_v1p2p1N',
-    'CICADA_v2p2p1N_Ntuplizer/CICADA_v2p2p1N',
-    'CICADA_vXp2p0_Teacher_Ntuplizer/CICADA_vXp2p0_teacher',
-    'CICADA_vXp2p0N_Teacher_Ntuplizer/CICADA_vXp2p0N_teacher',
-    'CICADA_vXp2p1_Teacher_Ntuplizer/CICADA_vXp2p1_teacher',
-    'CICADA_vXp2p1N_Teacher_Ntuplizer/CICADA_vXp2p1N_teacher'
+    #'CICADA_v1p2p0_Ntuplizer/CICADA_v1p2p0',
+    #'CICADA_v2p2p0_Ntuplizer/CICADA_v2p2p0',
+    #'CICADA_v1p2p0N_Ntuplizer/CICADA_v1p2p0N',
+    #'CICADA_v2p2p0N_Ntuplizer/CICADA_v2p2p0N',
+    #'CICADA_v1p2p1_Ntuplizer/CICADA_v1p2p1',
+    #'CICADA_v2p2p1_Ntuplizer/CICADA_v2p2p1',
+    #'CICADA_v1p2p1N_Ntuplizer/CICADA_v1p2p1N',
+    #'CICADA_v2p2p1N_Ntuplizer/CICADA_v2p2p1N',
+    'CICADA_v1p2p2_Ntuplizer/CICADA_v1p2p2',
+    'CICADA_v2p2p2_Ntuplizer/CICADA_v2p2p2',
+    'CICADA_v1p2p2N_Ntuplizer/CICADA_v1p2p2N',
+    'CICADA_v2p2p2N_Ntuplizer/CICADA_v2p2p2N'
+    #'CICADA_vXp2p0_Teacher_Ntuplizer/CICADA_vXp2p0_teacher',
+    #'CICADA_vXp2p0N_Teacher_Ntuplizer/CICADA_vXp2p0N_teacher',
+    #'CICADA_vXp2p1_Teacher_Ntuplizer/CICADA_vXp2p1_teacher',
+    #'CICADA_vXp2p1N_Teacher_Ntuplizer/CICADA_vXp2p1N_teacher'
 ]
 
 # function for building sample object from files
