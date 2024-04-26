bbe2cfe19e87631cadbe1448904bfc61faf6243c
Elliott Kauffman <elmaka8700@gmail.com>
2024-04-26 07:25:10 -0500

    updates to drawroc

--- drawROC.py
+++ drawROC.py
@@ -10,8 +10,15 @@ import argparse
 import numpy as np
 from array import array
 from sampleNames import sample_name_dict
+from efficiencies import efficiencies
 
 max_eff_val = 0.00344054
+
+# names of CICADA models
+cicada_names = ["CICADA_v1p2p2",
+                "CICADA_v2p2p2",
+                "CICADA_v1p2p2N",
+                "CICADA_v2p2p2N"]
 
 # function for drawing the CMS label on the plots
 def createCMSLabel():
@@ -22,88 +29,116 @@ def createCMSLabel():
 
     return cmsLatex
 
-def main(zb_file, signal_file, signal_name, cicada_version, out_dir):
+def main(file_prefix, out_dir):
 
-    f_zb = ROOT.TFile(zb_file)
-    f_s = ROOT.TFile(signal_file)
+    f_zb = ROOT.TFile(f"{file_prefix}_ZeroBias.root")
 
-    # load histograms
-    h_cicada_zb = f_zb.Get(f"anomalyScore_ZeroBias_test_{cicada_version}")
-    h_cicada_s = f_s.Get(f"anomalyScore_{signal_name}_{cicada_version}")
-    h_ht_zb = f_zb.Get(f"HT_ZeroBias_test")
-    h_ht_s = f_s.Get(f"HT_{signal_name}")
+    for k in range(len(cicada_names)):
 
-    # get integral of each histogram
-    integral_cicada_zb = float(h_cicada_zb.Integral(1, h_cicada_zb.GetNbinsX()+1))
-    integral_cicada_s = float(h_cicada_s.Integral(1, h_cicada_s.GetNbinsX()+1))
-    integral_ht_zb = float(h_ht_zb.Integral(1, h_ht_zb.GetNbinsX()+1))
-    integral_ht_s = float(h_ht_s.Integral(1, h_ht_s.GetNbinsX()+1))
+        # load ZeroBias histograms
+        h_cicada_zb = f_zb.Get(f"anomalyScore_ZeroBias_test_{cicada_names[k]}")
+        h_ht_zb = f_zb.Get(f"HT_ZeroBias_test")
 
-    # initialize lists for tpr and fpr
-    tpr_cicada = []
-    fpr_cicada = []
-    tpr_ht = []
-    fpr_ht = []
+        # get integral of ZeroBias histograms
+        integral_cicada_zb = float(h_cicada_zb.Integral(1, h_cicada_zb.GetNbinsX()+1))
+        integral_ht_zb = float(h_ht_zb.Integral(1, h_ht_zb.GetNbinsX()+1))
 
-    # iterate through bins and compute tpr and fpr for each threshold
-    for j in range(h_cicada_zb.GetNbinsX()):
-        # compute partial sums of histograms
-        partial_cicada_zb = float(h_cicada_zb.Integral(j+1, h_cicada_zb.GetNbinsX()+1))
-        partial_cicada_s = float(h_cicada_s.Integral(j+1, h_cicada_s.GetNbinsX()+1))
-        partial_ht_zb = float(h_ht_zb.Integral(j+1, h_ht_zb.GetNbinsX()+1))
-        partial_ht_s = float(h_ht_s.Integral(j+1, h_ht_s.GetNbinsX()+1))
-
-        fpr_cicada_current = partial_cicada_zb/integral_cicada_zb
-        tpr_cicada_current = partial_cicada_s/integral_cicada_s
-        if fpr_cicada_current <= max_eff_val:
-            fpr_cicada.append(fpr_cicada_current)
-            tpr_cicada.append(tpr_cicada_current)
-
-        fpr_ht_current = partial_ht_zb/integral_ht_zb
-        tpr_ht_current = partial_ht_s/integral_ht_s
-        if fpr_ht_current <= max_eff_val:
-            fpr_ht.append(fpr_ht_current)
-            tpr_ht.append(tpr_ht_current)
+        sample_names = list(sample_name_dict.keys())
+        for i in range(len(sample_names)):
 
 
-    # convert to np arrays for some reason
-    tpr_cicada = np.array(tpr_cicada)
-    fpr_cicada = np.array(fpr_cicada)
-    tpr_ht = np.array(tpr_ht)
-    fpr_ht = np.array(fpr_ht)
+            f_s = ROOT.TFile(f"{file_prefix}_{sample_names[i]}.root")
 
-    # create ROOT canvas
-    c = ROOT.TCanvas("c", "ROC", 1000, 800)
+            # load histograms
+            h_cicada_s = f_s.Get(f"anomalyScore_{sample_names[i]}_{cicada_names[k]}")
+            h_ht_s = f_s.Get(f"HT_{sample_names[i]}")
 
-    # create TGraph for cicada
-    g_cicada = ROOT.TGraph(len(tpr_cicada), fpr_cicada, tpr_cicada)
+            # get integral of each histogram
+            integral_cicada_s = float(h_cicada_s.Integral(1, h_cicada_s.GetNbinsX()+1))
+            integral_ht_s = float(h_ht_s.Integral(1, h_ht_s.GetNbinsX()+1))
 
-    # plotting options for cicada graph
-    g_cicada.SetTitle(f"ROC: Signal = {signal_name}, CICADA version = {cicada_version}")
-    g_cicada.SetLineWidth(4)
-    g_cicada.SetLineColor(2)
-    g_cicada.GetXaxis().SetTitle("FPR")
-    g_cicada.GetYaxis().SetTitle("TPR")
-    g_cicada.GetXaxis().SetRangeUser(0,max_eff_val)
-    g_cicada.GetYaxis().SetRangeUser(0,1.2)
+            # initialize lists for tpr and fpr
+            tpr_cicada = []
+            tpr_ht = []
+            fpr_cicada = []
+            fpr_ht = []
 
-    # draw cicada TGraph
-    g_cicada.Draw("AC")
+            # iterate through bins and compute tpr and fpr for each threshold
+            for j in range(h_cicada_zb.GetNbinsX()):
+                # compute partial sums of histograms
+                partial_cicada_s = float(h_cicada_s.Integral(j+1, h_cicada_s.GetNbinsX()+1))
+                partial_ht_s = float(h_ht_s.Integral(j+1, h_ht_s.GetNbinsX()+1))
+                partial_cicada_zb = float(h_cicada_zb.Integral(j+1, h_cicada_zb.GetNbinsX()+1))
+                partial_ht_zb = float(h_ht_zb.Integral(j+1, h_ht_zb.GetNbinsX()+1))
 
-    # create TGraph for HT
-    g_ht = ROOT.TGraph(len(tpr_ht), fpr_ht, tpr_ht)
+                tpr_cicada_current = partial_cicada_s/integral_cicada_s
+                fpr_cicada_current = partial_cicada_zb/integral_cicada_zb
+                if fpr_cicada_current <= max_eff_val:
+                    tpr_cicada.append(tpr_cicada_current)
+                    fpr_cicada.append(fpr_cicada_current)
 
-    # plotting options for ht graph
-    g_ht.SetLineWidth(4)
-    g_ht.SetLineColor(4)
-    g_ht.GetXaxis().SetRangeUser(0,max_eff_val)
-    g_ht.GetYaxis().SetRangeUser(0,1.2)
-    g_ht.Draw("C same")
+                fpr_ht_current = partial_ht_zb/integral_ht_zb
+                tpr_ht_current = partial_ht_s/integral_ht_s
+                if fpr_ht_current <= max_eff_val:
+                    tpr_ht.append(tpr_ht_current)
+                    fpr_ht.append(fpr_cicada_current)
 
-    # draw canvas and save plot
-    c.Draw()
-    c.SaveAs(f"{out_dir}/ROC_{cicada_version}_{signal_name}.png")
-    c.Close()
+
+            # convert to np arrays for some reason
+            tpr_cicada = np.array(tpr_cicada, dtype = np.double)
+            tpr_ht = np.array(tpr_ht, dtype = np.double)
+
+            # create ROOT canvas
+            c = ROOT.TCanvas("c", "ROC", 1000, 800)
+
+
+            # create TGraph for cicada
+            g_cicada = ROOT.TGraph(len(tpr_cicada), fpr_cicada, tpr_cicada)
+
+            # plotting options for cicada graph
+            g_cicada.SetTitle(f"ROC: Signal = {sample_names[i]}, CICADA version = {cicada_names[k]}")
+            g_cicada.SetLineWidth(4)
+            g_cicada.SetLineColor(2)
+            g_cicada.GetXaxis().SetTitle("Zero Bias Rate")
+            g_cicada.GetYaxis().SetTitle("Signal Efficiency")
+            g_cicada.GetXaxis().SetRangeUser(0,max_eff_val)
+            g_cicada.GetYaxis().SetRangeUser(0,1.2)
+
+            # draw cicada TGraph
+            g_cicada.Draw("AC")
+
+            # create TGraph for HT
+            g_ht = ROOT.TGraph(len(tpr_ht), fpr_ht, tpr_ht)
+
+            # plotting options for ht graph
+            g_ht.SetLineWidth(4)
+            g_ht.SetLineColor(4)
+            g_ht.GetXaxis().SetRangeUser(0,max_eff_val)
+            g_ht.GetYaxis().SetRangeUser(0,1.2)
+            g_ht.Draw("C same")
+
+            #g_l1 = ROOT.TGraph(1, [efficiencies["ZeroBias"]], [efficiencies[sample_names[i]]])
+            if sample_names[i] in efficiencies:
+                g_l1 = ROOT.TGraph(1, 0.5, [efficiencies[sample_names[i]]])
+            else: g_l1 = ROOT.TGraph(1, 0.5, 0.5)
+            g_l1.SetMarkerSize(5)
+            g_l1.Draw("P same")
+
+            legend = ROOT.TLegend(0.1,0.75,0.9, 0.95)
+            legend.AddEntry(g_cicada, "CICADA")
+            legend.AddEntry(g_ht, "HT")
+            legend.AddEntry(g_l1, "Unprescaled Trigger Efficiency")
+            legend.SetBorderSize(0)
+            legend.SetFillStyle(0)
+            legend.Draw()
+
+            # draw canvas and save plot
+            c.Draw()
+            c.SaveAs(f"{out_dir}/ROC_ZeroBias_{cicada_names[k]}_{sample_names[i]}.png")
+            c.Close()
+
+            #except Exception:
+                #print(f"Unable to access sample: {sample_names[i]}, CICADA version: {cicada_names[k]}")
 
 
 if __name__ == "__main__":
@@ -112,24 +147,9 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "-z",
-        "--zero_bias_file",
-        help="path to file containing zero bias histograms"
-    )
-    parser.add_argument(
-        "-s",
-        "--signal_file",
-        help="path to file containing signal histograms"
-    )
-    parser.add_argument(
-        "-n",
-        "--signal_name",
-        help="name of signal sample"
-    )
-    parser.add_argument(
-        "-c",
-        "--cicada_version",
-        help="CICADA version"
+        "-p",
+        "--file_prefix",
+        help="prefix of files containing histograms"
     )
     parser.add_argument(
         "-o",
@@ -142,9 +162,6 @@ if __name__ == "__main__":
 
 
     main(
-        args.zero_bias_file,
-        args.signal_file,
-        args.signal_name,
-        args.cicada_version,
+        args.file_prefix,
         args.output_dir
     )
