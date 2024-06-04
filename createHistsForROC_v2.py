0a614599f882fc791ee5a26ad8757bc1aaa66e46
Elliott Kauffman <elmaka8700@gmail.com>
2024-06-04 16:29:05 -0500

    work on 3d histogram solution

---
+++ createHistsForROC_v2.py
@@ -0,0 +1,245 @@
+########################################################################
+## createHistsForROC.py                                               ##
+## Author: Elliott Kauffman                                           ##
+## make score and rate histograms for CICADA                          ##
+########################################################################
+
+import ROOT
+import argparse
+import numpy as np
+from paperSampleBuilder import samples
+from triggers import triggers as trig_list
+import json
+import uproot
+import awkward as ak
+
+
+# names of CICADA models
+cicada_names = ["CICADA_v1p2p2"]
+                #"CICADA_v2p2p2",
+                #"CICADA_v1p2p2N",
+                #"CICADA_v2p2p2N"]
+
+# minimum and maximum scores for histogram
+min_score_cicada = 0.0
+max_score_cicada = 1024.0
+n_bins_cicada = 300
+min_score_ht = 0.0
+max_score_ht = 2000.0
+n_bins_ht = 200
+min_score_trig = -0.5
+max_score_trig = 1.5
+n_bins_trig = 2
+
+
+# Create a C++ lambda function for OR operation as a string
+or_operation_cpp = """
+int OROperation(const ROOT::VecOps::RVec<int>& cols) {
+    for (auto col : cols) {
+        if (col!=0) return 1;
+    }
+    return 0;
+}
+"""
+
+def getUnprescaledTriggers(triggerlist):
+
+    with open("filePaths.json") as f:
+        f = open("filePaths.json")
+        filePaths = json.load(f)["ZeroBias"]
+
+    j = 0
+    for filePath in filePaths:
+        f = uproot.open(filePath)
+        tree = f["L1TTriggerBitsNtuplizer/L1TTriggerBits"]
+        to_remove = []
+
+        for i in range(len(triggerlist)):
+
+            try:
+                prescales = tree[f"{triggerlist[i]}_prescale"].array()
+            except Exception:
+                print("Trigger not found: ", triggerlist[i])
+                continue
+            if len(prescales)==0: continue
+            if (sum(prescales)/len(prescales)!=1):
+                to_remove.append(triggerlist[i])
+
+        for i in range(len(to_remove)):
+            triggerlist.remove(to_remove[i])
+
+        j+=1
+        if j > 30: break
+
+    with open("unprescaledTriggers.json", "w") as final:
+        json.dump(triggerlist, final)
+
+
+    return triggerlist
+
+
+
+def main(out_prefix):
+    # get unprescaled triggers (filter out prescaled triggers from list)
+    with open("unprescaledTriggerTables.json", "r") as f:
+        triggers = json.load(f)["unprescaledTriggers"]
+
+    # Create a C++ function dynamically for the OR operation
+    cpp_code = """
+    #include <ROOT/RVec.hxx>
+    using namespace ROOT::VecOps;
+
+    int OROperation(const RVec<int>& cols) {
+        for (auto col : cols) {
+            if (col != 0) return 1;
+        }
+        return 0;
+    }
+    """
+
+    # compile cpp code
+    ROOT.gInterpreter.Declare(cpp_code)
+
+    columns_str = ", ".join(triggers)
+    function_call = f"OROperation({{{columns_str}}})"
+
+    # get list of sample names and remove ZeroBias
+    sample_names = list(samples.keys())
+    if 'ZeroBias' in sample_names:
+        sample_names.remove('ZeroBias')
+
+    # get zerobias and filter out training events
+    zero_bias = samples['ZeroBias'].getNewDataframe()
+
+    # create column for HT
+    zero_bias = zero_bias.Define("sum_mask", "(sumBx==0) && (sumType==1)")
+    zero_bias = zero_bias.Define("HT", "sumEt[sum_mask]")
+
+    # create column for whether events pass any unprescaled triggers
+    zero_bias = zero_bias.Define("L1UnprescaledOR", function_call)
+
+    # separate out test and train data
+    zero_bias_test = zero_bias.Filter('lumi % 2 == 1')
+    zero_bias_train = zero_bias.Filter('lumi % 2 == 0')
+
+    #array = ak.from_rdataframe(zero_bias_test, columns=("L1UnprescaledOR",))
+    #print(array)
+    l1unprescaledor = zero_bias_test.AsNumpy(columns=["L1UnprescaledOR"])
+    print("ZB Efficiency = ", sum(l1unprescaledor["L1UnprescaledOR"])/len(l1unprescaledor["L1UnprescaledOR"]))
+
+
+
+    # create file for zerobias hists
+    print("Sample: ZeroBias")
+    output_file = ROOT.TFile(f"{out_prefix}_ZeroBias.root", "RECREATE")
+
+    # create and fill CICADA score histograms for zerobias
+    for i in range(len(cicada_names)):
+        print("    CICADA VERSION: ", cicada_names[i])
+
+        histModel = ROOT.RDF.TH3DModel(
+            f"anomalyScore_ZeroBias_test_{cicada_names[i]}",
+            f"anomalyScore_ZeroBias_test_{cicada_names[i]}",
+            n_bins_cicada,
+            min_score_cicada,
+            max_score_cicada,
+            n_bins_ht,
+            min_score_ht,
+            max_score_ht,
+            n_bins_trig,
+            min_score_trig,
+            max_score_trig,
+        )
+
+        hist = zero_bias_test.Histo3D(
+            histModel,
+            f"{cicada_names[i]}_score",
+            "HT",
+            "L1UnprescaledOR"
+        )
+        print("        Bin Content Example 1 = ", hist.GetBinContent(10, 2, 2))
+        print("        Bin Content Example 2 = ", hist.GetBinContent(5, 1, 1))
+        hist.Write()
+
+
+        histModel = ROOT.RDF.TH3DModel(
+            f"anomalyScore_ZeroBias_train_{cicada_names[i]}",
+            f"anomalyScore_ZeroBias_train_{cicada_names[i]}",
+            n_bins_cicada,
+            min_score_cicada,
+            max_score_cicada,
+            n_bins_ht,
+            min_score_ht,
+            max_score_ht,
+            n_bins_trig,
+            min_score_trig,
+            max_score_trig
+        )
+
+        hist = zero_bias_train.Histo3D(
+            histModel,
+            f"{cicada_names[i]}_score",
+            "HT",
+            "L1UnprescaledOR"
+        )
+        hist.Write()
+
+    output_file.Write()
+    output_file.Close()
+
+
+    # create and write hists for each sample
+    for k in range(len(sample_names)):
+        print(f"Sample: {sample_names[k]}")
+        output_file = ROOT.TFile(f"{out_prefix}_{sample_names[k]}.root", "RECREATE")
+
+        rdf = samples[sample_names[k]].getNewDataframe()
+        rdf = rdf.Define("sum_mask", "(sumBx==0) && (sumType==1)")
+        rdf = rdf.Define("HT", "sumEt[sum_mask]")
+
+        rdf = rdf.Define("L1UnprescaledOR", function_call)
+
+        l1unprescaledor = rdf.AsNumpy(columns=["L1UnprescaledOR"])
+        print("    Sig Efficiency = ", sum(l1unprescaledor["L1UnprescaledOR"])/len(l1unprescaledor["L1UnprescaledOR"]))
+
+        # create and fill CICADA score histograms
+        for i in range(len(cicada_names)):
+            print("    CICADA VERSION: ", cicada_names[i])
+
+            histModel = ROOT.RDF.TH3DModel(
+                f"anomalyScore_{sample_names[k]}_{cicada_names[i]}",
+                f"anomalyScore_{sample_names[k]}_{cicada_names[i]}",
+                n_bins_cicada,
+                min_score_cicada,
+                max_score_cicada,
+                n_bins_ht,
+                min_score_ht,
+                max_score_ht,
+                n_bins_trig,
+                min_score_trig,
+                max_score_trig
+            )
+
+            hist = rdf.Histo3D(
+                histModel,
+                f"{cicada_names[i]}_score",
+                "HT",
+                "L1UnprescaledOR"
+            )
+            print("        Bin Content Example 1 = ", hist.GetBinContent(10, 2, 2))
+            print("        Bin Content Example 2 = ", hist.GetBinContent(5, 1, 1))
+            hist.Write()
+
+        output_file.Write()
+        output_file.Close()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="This program creates 3d histograms with CICADA score, HT, and L1 Unprescaled OR"
+    )
+    parser.add_argument("-o", "--out_prefix", default = "ROChist", help="output file name/path prefix")
+    args = parser.parse_args()
+
+    main(args.out_prefix)
---
+++ drawROC_v4.py
@@ -0,0 +1,411 @@
+########################################################################
+## drawROC.py                                                         ##
+## Author: Elliott Kauffman                                           ##
+## takes output histograms from createHistsForROC.py and creates ROC  ##
+## plots                                                              ##
+########################################################################
+
+import ROOT
+import argparse
+import numpy as np
+from array import array
+from sampleNames import sample_name_dict
+from efficiencies import efficiencies
+import ctypes
+
+max_eff_val_x = 0.00344054 * 2
+max_eff_val_x = 1.0
+max_eff_val_y = 1.2
+
+
+#rate_scale_factor = 2544.0 * 11245e-3
+
+ht_threshold = 200
+l1_threshold = 1
+
+# names of CICADA models
+cicada_names = ["CICADA_v1p2p2"]
+                #"CICADA_v2p2p2",
+                #"CICADA_v1p2p2N",
+                #"CICADA_v2p2p2N"]
+cicada_names_print = ["CICADA 1.2.2",
+                      "CICADA 2.2.2",
+                      "CICADA 1.2.2N",
+                      "CICADA 2.2.2N"]
+
+# function for drawing the label on the plots
+def createLabel():
+    latex = ROOT.TLatex()
+    latex.SetTextSize(0.03)
+    latex.SetNDC(True)
+    latex.SetTextAlign(11)
+
+    return latex
+
+def calculateROC(bkg_hist, sig_hist):
+
+    # get total integral of background histogram
+    integral_bkg = float(
+        bkg_hist.Integral(0, bkg_hist.GetNbinsX()+1)
+    )
+
+    # get total integral of signal histogram
+    integral_sig = float(
+        sig_hist.Integral(0, sig_hist.GetNbinsX()+1)
+    )
+
+    edges = [sig_hist.GetXaxis().GetBinLowEdge(i) for i in range(1, sig_hist.GetNbinsX() + 2)]
+
+    tpr = []
+    fpr = []
+    for threshold in edges:
+        threshold_bin = sig_hist.GetXaxis().FindBin(threshold)
+        integral_sig_partial = sig_hist.Integral(threshold_bin, sig_hist.GetNbinsX() + 1)
+        integral_bkg_partial = bkg_hist.Integral(threshold_bin, bkg_hist.GetNbinsX() + 1)
+
+        tpr_current = integral_sig_partial / integral_sig
+        fpr_current = integral_bkg_partial / integral_bkg
+
+        tpr.append(tpr_current)
+        fpr.append(fpr_current)
+
+    tpr = array('d', tpr)
+    fpr = array('d', fpr)
+
+    return tpr, fpr
+
+def calculateROCOR(bkg_hist, sig_hist, or_threshold, or_axis):
+
+    # get total integral of background histogram
+    integral_bkg = float(
+        bkg_hist.Integral(0, bkg_hist.GetNbinsX()+1,
+                          0, bkg_hist.GetNbinsY()+1,
+                          0, bkg_hist.GetNbinsZ()+1)
+    )
+
+    # get total integral of signal histogram
+    integral_sig = float(
+        sig_hist.Integral(0, sig_hist.GetNbinsX()+1,
+                          0, sig_hist.GetNbinsY()+1,
+                          0, sig_hist.GetNbinsZ()+1)
+    )
+
+    if or_axis not in [1,2]:
+        raise ValueError("or_axis must be 1 or 2")
+    if or_axis == 1:
+        or_threshold_bin = sig_hist.GetYaxis().FindBin(or_threshold)
+    else:
+        or_threshold_bin = sig_hist.GetYaxis().FindBin(or_threshold)
+
+    edges = [sig_hist.GetXaxis().GetBinLowEdge(i) for i in range(1, sig_hist.GetNbinsX() + 2)]
+
+    tpr = []
+    fpr = []
+    for threshold in edges:
+        threshold_bin = sig_hist.GetXaxis().FindBin(threshold)
+
+        # Integrate over bins where x > threshold, including overflow bins
+        integral_sig_partial_x = sig_hist.Integral(threshold_bin, sig_hist.GetNbinsX() + 1,
+                                                   0, sig_hist.GetNbinsY() + 1,
+                                                   0, sig_hist.GetNbinsZ() + 1)
+        integral_bkg_partial_x = bkg_hist.Integral(threshold_bin, bkg_hist.GetNbinsX() + 1,
+                                                   0, bkg_hist.GetNbinsY() + 1,
+                                                   0, bkg_hist.GetNbinsZ() + 1)
+
+        if or_axis == 1:
+            # Integrate over bins where y or z > or_threshold, including overflow bins
+            integral_sig_partial_yz = sig_hist.Integral(0, sig_hist.GetNbinsX() + 1,
+                                                        or_threshold_bin, sig_hist.GetNbinsY() + 1,
+                                                        0, sig_hist.GetNbinsZ() + 1)
+            integral_bkg_partial_yz = bkg_hist.Integral(0, bkg_hist.GetNbinsX() + 1,
+                                                        or_threshold_bin, bkg_hist.GetNbinsY() + 1,
+                                                        0, bkg_hist.GetNbinsZ() + 1)
+            # Compute the double-counted accepted events, including overlap and overflow bins
+            overlap_sig = sig_hist.Integral(threshold_bin, sig_hist.GetNbinsX() + 1,
+                                            or_threshold_bin, sig_hist.GetNbinsY() + 1,
+                                            0, sig_hist.GetNbinsZ() + 1)
+            overlap_bkg = bkg_hist.Integral(threshold_bin, bkg_hist.GetNbinsX() + 1,
+                                            or_threshold_bin, bkg_hist.GetNbinsY() + 1,
+                                            0, bkg_hist.GetNbinsZ() + 1)
+            
+        else:
+            # Integrate over bins where y or z > or_threshold, including overflow bins
+            integral_sig_partial_yz = sig_hist.Integral(0, sig_hist.GetNbinsX() + 1,
+                                                        0, sig_hist.GetNbinsY() + 1,
+                                                        or_threshold_bin, sig_hist.GetNbinsZ() + 1)
+            integral_bkg_partial_yz = bkg_hist.Integral(0, bkg_hist.GetNbinsX() + 1,
+                                                        0, bkg_hist.GetNbinsY() + 1,
+                                                        or_threshold_bin, bkg_hist.GetNbinsZ() + 1)
+            # Compute the double-counted accepted events, including overlap and overflow bins
+            overlap_sig = sig_hist.Integral(threshold_bin, sig_hist.GetNbinsX() + 1,
+                                            0, sig_hist.GetNbinsY() + 1,
+                                            or_threshold_bin, sig_hist.GetNbinsZ() + 1)
+            overlap_bkg = bkg_hist.Integral(threshold_bin, bkg_hist.GetNbinsX() + 1,
+                                            0, bkg_hist.GetNbinsY() + 1,
+                                            or_threshold_bin, bkg_hist.GetNbinsZ() + 1)
+
+        
+
+        accepted_sig = integral_sig_partial_x + integral_sig_partial_yz - overlap_sig
+        accepted_bkg = integral_bkg_partial_x + integral_bkg_partial_yz - overlap_bkg
+
+        tpr_current = accepted_sig / integral_sig
+        fpr_current = accepted_bkg / integral_bkg
+
+        tpr.append(tpr_current)
+        fpr.append(fpr_current)
+
+    tpr = array('d', tpr)
+    fpr = array('d', fpr)
+
+    return tpr, fpr
+
+def createROCTGraph(tpr, fpr, color = 1, markerstyle = 20, first = True, bkg_name = "Zero Bias"):
+
+    # create TGraph
+    g = ROOT.TGraph(len(tpr), fpr, tpr)
+
+    # plotting options graph
+    g.SetTitle("")
+    g.SetLineWidth(4)
+    g.SetLineColor(color)
+    g.SetMarkerColor(color)
+    g.SetMarkerSize(1)
+    g.SetMarkerStyle(markerstyle)
+    if first:
+        g.GetXaxis().SetTitle(f"{bkg_name} Efficiency (FPR)")
+        g.GetYaxis().SetTitle("Signal Efficiency")
+    g.GetXaxis().SetRangeUser(0, max_eff_val_x)
+    #g.GetXaxis().SetLimits(0, max_eff_val_x)
+    g.GetYaxis().SetRangeUser(0, max_eff_val_y)
+    
+    return g
+
+def getL1UnprescaledEfficiency(hist):
+
+    hist_projection = hist.ProjectionZ("l1_unprescaled", 0, hist.GetNbinsX()+1, 0, hist.GetNbinsY()+1)
+    n_fail = hist_projection.GetBinContent(1)
+    n_pass = hist_projection.GetBinContent(0)
+
+    return n_pass / (n_fail + n_pass)
+
+
+
+def main(file_prefix, out_dir):
+
+    f_zb = ROOT.TFile(f"{file_prefix}_ZeroBias.root")
+    f_sn = ROOT.TFile(f"{file_prefix}_SingleNeutrino_E-10-gun.root")
+
+    f_bkg = [f_zb, f_sn]
+    bkg_names = ["ZeroBias", "SingleNeutrino_E-10-gun"]
+    bkg_names_print = ["Zero Bias", "Single Neutrino Gun"]
+
+    for l in range(len(f_bkg)):
+        for k in range(len(cicada_names)):
+
+            # load ZeroBias histograms
+            if bkg_names[l]=="ZeroBias":
+                h_zb = f_bkg[l].Get(f"anomalyScore_ZeroBias_test_{cicada_names[k]}")
+            else:
+                h_zb = f_bkg[l].Get(f"anomalyScore_SingleNeutrino_E-10-gun_{cicada_names[k]}")
+
+            sample_names = list(sample_name_dict.keys())
+            for i in range(len(sample_names)):
+
+                if sample_names[i]=="ZeroBias": continue
+                if sample_names[i]=="SingleNeutrino_E-10-gun": continue
+                if sample_names[i]!="GluGluHToBB_M-125_TuneCP5_13p6TeV_powheg-pythia8": continue
+
+                print()
+                print(sample_names[i])
+
+                try:
+                    f_s = ROOT.TFile(f"{file_prefix}_{sample_names[i]}.root")
+                except Exception:
+                    print(f"ROOT file for sample {sample_names[i]} does not exist")
+                    continue
+
+                # load histograms
+                h_s = f_s.Get(f"anomalyScore_{sample_names[i]}_{cicada_names[k]}")
+                ht_bin = h_s.GetYaxis().FindBin(200)
+
+                tpr_cicada, fpr_cicada = calculateROC(
+                    h_zb.ProjectionX("CICADA_score", 0, h_zb.GetNbinsY()+1),
+                    h_s.ProjectionX("CICADA_score", 0, h_s.GetNbinsY()+1)
+                )
+                print("TPR (CICADA) = ", tpr_cicada)
+                print("FPR (CICADA) = ", fpr_cicada, "\n")
+
+                tpr_ht, fpr_ht = calculateROC(
+                    h_zb.ProjectionY("HT", 0, h_zb.GetNbinsX()+1),
+                    h_s.ProjectionY("HT", 0, h_s.GetNbinsX()+1)
+                )
+                print("TPR (HT) = ", tpr_ht)
+                print("FPR (HT) = ", fpr_ht, "\n")
+
+                try:
+                    tpr_or_ht, fpr_or_ht = calculateROCOR(h_zb, h_s, ht_threshold, 1)
+                except ValueError as e:
+                    print("Error:", e)
+                print("TPR (HT OR CICADA) = ", tpr_or_ht)
+                print("FPR (HT OR CICADA) = ", fpr_or_ht, "\n")
+
+                try:
+                    tpr_or_l1, fpr_or_l1 = calculateROCOR(h_zb, h_s, l1_threshold, 2)
+                except ValueError as e:
+                    print("Error:", e)
+                print("TPR (L1 OR CICADA) = ", tpr_or_l1)
+                print("FPR (L1 OR CICADA) = ", fpr_or_l1, "\n")
+
+
+                ########################################################################
+                # plot for HT OR                                                       #
+                ########################################################################
+
+                # create ROOT canvas
+                c = ROOT.TCanvas("c", "ROC", 1000, 800)
+
+                # draw TGraph for HT OR CICADA score
+                g_or = createROCTGraph(tpr_or_ht,
+                                       fpr_or_ht,
+                                       color = 6,
+                                       markerstyle = 20,
+                                       first = True,
+                                       bkg_name = bkg_names_print[l])
+                g_or.Draw("AP")
+
+                # draw TGraph for CICADA score
+                g_cicada = createROCTGraph(tpr_cicada,
+                                           fpr_cicada,
+                                           color = 8,
+                                           markerstyle = 21,
+                                           first = False)
+                g_cicada.Draw("P same")
+
+                # draw TGraph for HT
+                g_ht = createROCTGraph(tpr_ht,
+                                       fpr_ht,
+                                       color = 9,
+                                       markerstyle = 22,
+                                       first = False)
+                g_ht.Draw("P same")
+
+                # draw graph title
+                title_template = "#splitline{{Signal = {str_sig}}}{{CICADA Version = {str_cic}}}"
+                title = title_template.format(str_sig = sample_name_dict[sample_names[i]], str_cic = cicada_names_print[k])
+                titleObj = createLabel()
+                titleObj.DrawLatex(0.1, 0.93, title)
+
+                # plot point for l1 unprescaled efficiency
+                l1_eff_s = getL1UnprescaledEfficiency(h_s)
+                l1_eff_zb = getL1UnprescaledEfficiency(h_s)
+                print("L1 Unprescaled Signal Efficiency = ", l1_eff_s)
+                print("L1 Unprescaled Bkg Efficiency = ", l1_eff_zb)
+                g_l1 = ROOT.TGraph(1, array('d', [l1_eff_zb]), array('d', [l1_eff_s]))
+                g_l1.SetMarkerSize(1)
+                g_l1.SetMarkerStyle(20)
+                g_l1.GetXaxis().SetRangeUser(0, max_eff_val_x)
+                g_l1.GetYaxis().SetRangeUser(0, max_eff_val_y)
+                g_l1.Draw("P same")
+
+                # draw legend
+                legend = ROOT.TLegend(0.1,0.77,0.9, 0.9)
+                legend.AddEntry(g_or, "(CICADA > Threshold) or (HT > 200) ")
+                legend.AddEntry(g_cicada, "CICADA > Threshold")
+                legend.AddEntry(g_ht, "HT > 200")
+                legend.AddEntry(g_l1, "Unprescaled Trigger Efficiency", "p")
+                legend.SetBorderSize(0)
+                legend.SetFillStyle(0)
+                legend.SetTextSize(0.025)
+                legend.Draw()
+
+                # draw canvas and save plot
+                c.Update()
+                c.Draw()
+                c.SaveAs(f"{out_dir}/ROC_HT_{bkg_names[l]}_{cicada_names[k]}_{sample_names[i]}.png")
+                c.Close()
+
+                ########################################################################
+                # plot for L1 OR                                                       #
+                ########################################################################
+
+                # create ROOT canvas
+                c = ROOT.TCanvas("c", "ROC", 1000, 800)
+
+                # draw TGraph for L1 OR CICADA score
+                g_or = createROCTGraph(tpr_or_l1,
+                                       fpr_or_l1,
+                                       color = 6,
+                                       markerstyle = 20,
+                                       first = True,
+                                       bkg_name = bkg_names_print[l])
+                g_or.Draw("AP")
+
+                # draw TGraph for CICADA score
+                g_cicada = createROCTGraph(tpr_cicada,
+                                           fpr_cicada,
+                                           color = 8,
+                                           markerstyle = 21,
+                                           first = False)
+                g_cicada.Draw("P same")
+
+                # draw graph title
+                title_template = "#splitline{{Signal = {str_sig}}}{{CICADA Version = {str_cic}}}"
+                title = title_template.format(str_sig = sample_name_dict[sample_names[i]], str_cic = cicada_names_print[k])
+                titleObj = createLabel()
+                titleObj.DrawLatex(0.1, 0.93, title)
+
+                # plot point for l1 unprescaled efficiency
+                l1_eff_s = getL1UnprescaledEfficiency(h_s)
+                l1_eff_zb = getL1UnprescaledEfficiency(h_s)
+                print("L1 Unprescaled Signal Efficiency = ", l1_eff_s)
+                print("L1 Unprescaled Bkg Efficiency = ", l1_eff_zb)
+                g_l1 = ROOT.TGraph(1, array('d', [l1_eff_zb]), array('d', [l1_eff_s]))
+                g_l1.SetMarkerSize(1)
+                g_l1.SetMarkerStyle(20)
+                g_l1.GetXaxis().SetRangeUser(0, max_eff_val_x)
+                g_l1.GetYaxis().SetRangeUser(0, max_eff_val_y)
+                g_l1.Draw("P same")
+
+                # draw legend
+                legend = ROOT.TLegend(0.1,0.77,0.9, 0.9)
+                legend.AddEntry(g_or, "(CICADA > Threshold) or (Passed an Unprescaled L1 Trigger)")
+                legend.AddEntry(g_cicada, "CICADA > Threshold")
+                legend.AddEntry(g_l1, "Unprescaled Trigger Efficiency", "p")
+                legend.SetBorderSize(0)
+                legend.SetFillStyle(0)
+                legend.SetTextSize(0.025)
+                legend.Draw()
+
+                # draw canvas and save plot
+                c.Update()
+                c.Draw()
+                c.SaveAs(f"{out_dir}/ROC_L1_{bkg_names[l]}_{cicada_names[k]}_{sample_names[i]}.png")
+                c.Close()
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="This program creates CICADA ROC plots"
+    )
+
+    parser.add_argument(
+        "-p",
+        "--file_prefix",
+        help="prefix of files containing histograms"
+    )
+    parser.add_argument(
+        "-o",
+        "--output_dir",
+        default='./',
+        help="directory to save output plots"
+    )
+
+    args = parser.parse_args()
+
+
+    main(
+        args.file_prefix,
+        args.output_dir
+    )
